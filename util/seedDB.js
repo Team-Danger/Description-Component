ac7ae296730afd776776c3e9e002b234f5499d07
ertdfgcb <odaypaul@gmail.com>
2020-06-19 11:19:55 -0700

    move strings to seperate file and finish listing generation

--- util/seedDB.js
+++ util/seedDB.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const _ = require('lodash');
+const { bedStrings, amenityStrings, titleStrings } = require('./seedStrings.js');
+
+// call fn(time) a random number of times from max to min (inclusive)
+function randomIterations(min, max, fn) {
+  _.range(1, _.random(min, max)).map(fn);
+}
 
 // returns a promise that will resolve to a user object
 async function generateUser(id) {
@@ -22,46 +28,23 @@ async function generateUser(id) {
 }
 
 function generateTitle() {
-  const adjective = _.sample([
-    'Cozy',
-    'Humble',
-    'Extravagant',
-    'Rustic',
-    'Private',
-    'Amazing',
-    'Extreme',
-  ]);
-  const place = _.sample([
-    'apartment',
-    'house',
-    'condo',
-    'cottage',
-    'villa',
-    'hovel',
-    'room',
-  ]);
-  const location = _.sample([
-    'Portland',
-    'Seattle',
-    'San Francisco',
-    'Forest Grove',
-    'Dodgeville',
-    'Corvallis',
-    'Philomath',
-  ]);
+  const adjective = _.sample(titleStrings.adjective);
+  const place = _.sample(titleStrings.place);
+  const location = _.sample(titleStrings.location);
   return `${adjective} ${place} in ${location}`;
 }
 
-function generateDescription() {
-  return axios.get('https://baconipsum.com/api/?type=meat-and-filler&format=text');
+function generateIpsum(sentances = 10) {
+  return axios.get(`https://baconipsum.com/api/?type=meat-and-filler&format=text&sentances=${sentances}`);
 }
 
 function generateArrangements() {
   let hasCommonArea = false;
-  return _.range(1, _.random(2, 10)).map((index) => {
-    // every iteration we have a 1 in 3 chance of making a common area
+  const arrangements = [];
+  randomIterations(1, _.random(2, 10), (index) => {
     let location;
-    if (_.random(0, 3) && !hasCommonArea) {
+    // every iteration we have a 1 in 3 chance of making a common area
+    if (!_.random(0, 3) && !hasCommonArea) {
       hasCommonArea = true;
       location = 'Common Space';
     } else if (hasCommonArea) {
@@ -69,29 +52,41 @@ function generateArrangements() {
       // so we have to do this to make the bedrooms count up right
       location = `Bedroom${index - 1}`;
     } else {
+      // there's no common area so the index matches the bedroom number
       location = `Bedroom${index}`;
     }
-    const bed = _.sample([
-      'Double',
-      'Queen',
-      'Single',
-      'Sofa Bed',
-      'King',
-      'Small Double',
-      'Couch',
-      'Bunk Bed',
-      'Floor Mattress',
-      'Air Mattress',
-      'Crib',
-      'Toddler Bed',
-      'Hammock',
-      'Water Bed',
-    ]);
-    return {
+    const beds = [];
+    randomIterations(1, 10, () => {
+      const bed = _.sample(bedStrings);
+      beds.push({ amount: _.random(1, 5), bed });
+    });
+    arrangements.push({
       location,
-      bed,
-    };
+      beds,
+    });
   });
+  return arrangements;
+}
+
+// returns a promise resolving to a list of 5 to 20 non repeating amenities
+async function generateAmenities() {
+  const amenities = [];
+
+  randomIterations(5, 20, async () => {
+    const category = _.sample(amenityStrings);
+    // 50% chance of having a description
+    const description = _.random(0, 1)
+      ? await generateIpsum(1)
+      : '';
+    const amenity = ({
+      type: category.type,
+      amenity: _.sample(category.amenities),
+      description,
+    });
+    _.remove(category.amenities, (a) => a === amenity.amenity);
+    amenities.push(amenity);
+  });
+  return amenities;
 }
 
 // returns a promise that will resolve to a listing object
@@ -104,12 +99,14 @@ async function generateListing(listingId, userId) {
     listing_id: listingId,
     user: await generateUser(userId),
     title: generateTitle(),
-    description: await generateDescription(),
+    description: await generateIpsum(),
     guests: _.random(1, 10),
     bedrooms: _.random(0, 5),
     beds: _.random(1, 20),
     publicBaths,
     privateBaths,
+    sleepingArrangements: generateArrangements(),
+    amenities: generateAmenities(),
   };
 }
 
