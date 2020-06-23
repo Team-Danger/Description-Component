21e282c41fc3a4f7c5b0c1257df5d477796825e3
ertdfgcb <odaypaul@gmail.com>
2020-06-23 14:12:56 -0700

    add server for webpack'd files, and api call in react

--- db/db.js
+++ db/db.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 
 const mongoUri = 'mongodb://localhost/airbnbdesc';
 
-const db = mongoose.connect(mongoUri);
+const db = mongoose.connect(mongoUri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 const { connection } = mongoose;
 connection.on('error', Console.error.bind(console, 'connection error:'));
 connection.once('open', () => Console.log('connected to mongodb'));
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const Console = require('console');
+const path = require('path');
 const express = require('express');
 const Listing = require('../db/Listing');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/:id/description', (req, res) => {
   Console.log(`got request for ${req.params.id}`);
