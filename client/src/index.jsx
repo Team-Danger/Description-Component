bbd58e31e8a1ee70cff30cade3b69f3c869ce000
ertdfgcb <odaypaul@gmail.com>
2020-06-24 13:56:27 -0700

    fix typos

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Description from './components/Description';
 
-function endpoint(id) {
+function makeUrl(id) {
   return `http://localhost:3000/${id}/description`;
 }
 
@@ -12,7 +12,7 @@ function App({ endpoint }) {
 }
 
 App.propTypes = {
-  id: PropTypes.string.isRequired,
+  endpoint: PropTypes.string.isRequired,
 };
 
-ReactDOM.render(<App endpoint={endpoint('001')} />, document.getElementById('container'));
+ReactDOM.render(<App endpoint={makeUrl('001')} />, document.getElementById('container'));
