db84531c42c70451a593e5e4325ee7b0b90791c9
ertdfgcb <odaypaul@gmail.com>
2020-06-25 13:12:41 -0700

    rearrange index and description component

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -1,51 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import makeKey from '../../../util/makeKey';
 import Title from './Title';
 import Amenity from './Amenity';
 import SleepingArrangment from './SleepingArrangement';
 import User from './User';
 
-class Description extends React.Component {
-  componentDidMount() {
-    const { endpoint } = this.props;
-    axios.get(endpoint)
-      .then(({ data }) => {
-        this.setState(data);
-      })
-      .catch((err) => {
-        throw new Error(err);
-      });
-  }
+function makeAmenities(amenities) {
+  return amenities.map(({ amenity, description }) => (
+    <Amenity key={makeKey('desc')} type={amenity} description={description} />
+  ));
+}
 
-  render() {
-    if (this.state) {
-      const {
-        body, title, guests, bedrooms, beds, amenities, sleepingArrangements, user
-      } = this.state;
-      const amenityComponents = amenities.map(({ amenity, description }) => (
-        <Amenity key={makeKey('desc')} type={amenity} description={description} />
-      ));
-      const sleepingComponents = sleepingArrangements.map(({ location, beds: bedsList }) => (
-        <SleepingArrangment key={makeKey('desc')} location={location} beds={bedsList} />
-      ));
-      return (
-        <div>
-          <Title title={title} guests={guests} bedrooms={bedrooms} beds={beds} />
-          <User name={user.name} imageUrl={user.imageUrl} />
-          <div>{body}</div>
-          {sleepingComponents}
-          {amenityComponents}
-        </div>
-      );
-    }
-    return <div>no data</div>;
+function makeArrangements(sleepingArrangements) {
+  return sleepingArrangements.map(({ location, beds: bedsList }) => (
+    <SleepingArrangment key={makeKey('desc')} location={location} beds={bedsList} />
+  ));
+}
+
+function Description({ data }) {
+  if (data) {
+    const {
+      body, title, guests, bedrooms, beds, amenities, sleepingArrangements, user,
+    } = data;
+    const amenityComponents = makeAmenities(amenities);
+    const sleepingComponents = makeArrangements(sleepingArrangements);
+    return (
+      <div>
+        <Title title={title} guests={guests} bedrooms={bedrooms} beds={beds} />
+        <User name={user.name} imageUrl={user.imageUrl} />
+        <div>{body}</div>
+        {sleepingComponents}
+        {amenityComponents}
+      </div>
+    );
   }
+  return <div>no data</div>;
 }
 
 Description.propTypes = {
-  endpoint: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    body: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    guests: PropTypes.number.isRequired,
+    bedrooms: PropTypes.number.isRequired,
+    beds: PropTypes.number.isRequired,
+    amenities: PropTypes.arrayOf(PropTypes.shape(Amenity.propTypes)),
+    sleepingArrangements: PropTypes.arrayOf(PropTypes.shape(SleepingArrangment.propTypes)),
+    user: PropTypes.shape(User.propTypes),
+  }).isRequired,
 };
 
 export default Description;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Description from './components/Description';
@@ -7,8 +8,25 @@ function makeUrl(id) {
   return `http://localhost:3000/${id}/description`;
 }
 
-function App({ endpoint }) {
-  return <Description endpoint={endpoint} />;
+class App extends React.Component {
+  componentDidMount() {
+    const { endpoint } = this.props;
+    axios.get(endpoint)
+      .then(({ data }) => {
+        this.setState({ data });
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+    if (data) {
+      return <Description body={data} />;
+    }
+    return <div>no data</div>;
+  }
 }
 
 App.propTypes = {
